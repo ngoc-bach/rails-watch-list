f3163c1d394929e665156ead44dc918ced0ed2b4
Rachel Bach <thanhngocbach0904@gmail.com>
2022-11-17 15:56:41 +1100

    update rooth_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: [:index, :new, :create, :show, :destroy] do
+  root to: "lists#index"
+  resources :lists, only: [:new, :create, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
